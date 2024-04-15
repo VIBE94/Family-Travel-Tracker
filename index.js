e81787a02af26342214be29b0d1a4724e4fc8710
Tejasvi Arya <tejasvi.arya8955@gmail.com>
2024-04-15 23:56:52 +0530

    third commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ db.connect();
 const app = express();
 const port = 3000;
 app.use(express.static('public'))
-app.listen(process.env.PORT || 3000, function () {
+app.listen(3000,(req,res)=>{
   console.log("Server started on port 3000");
 })
 app.use(bodyParser.urlencoded({ extended: true }));
