159e3e2457c77a9c7fdccb3f143465705a6027da
Tejasvi Arya <tejasvi.arya8955@gmail.com>
2024-04-16 00:16:57 +0530

    jwbdfaqju

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ db.connect();
 
 
 const app = express();
-const port = 3000;
 app.use(express.static(__dirname+'public'))
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
